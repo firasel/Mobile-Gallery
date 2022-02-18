540c2f4a95b634bc396857ae4e2e7e8f68bbf05a
firasel <mdrasel601390@gmail.com>
2022-02-18 20:30:02 +0600

    Application initialize

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Hero from "./Components/Hero/Hero";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div>
+        <h2>Welcome to Mobile Gallery</h2>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
