260b1e975947e0888c3a7431d965c530d6c84620
firasel <mdrasel601390@gmail.com>
2022-02-19 05:39:25 +0600

    Navbar responsive done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-// import "./Navbar.css";
+import React, { useState } from "react";
+import { CgMenuRight } from "react-icons/cg";
 
 const Navbar = () => {
+  const [expand, setExpand] = useState(false);
+
+  console.log(expand);
   return (
     <nav className="container mx-auto">
       <div className="flex items-center justify-between">
@@ -15,7 +18,7 @@ const Navbar = () => {
             Gallery
           </div>
         </div>
-        <div className="flex items-center font-[Nunito]">
+        <div className="hidden md:block md:flex items-center font-[Nunito]">
           <span className="text-white opacity-60 hover:opacity-75 transition-all duration-200 cursor-pointer px-2 py-2">
             Location
           </span>
@@ -29,6 +32,30 @@ const Navbar = () => {
             Contact
           </span>
         </div>
+        <div
+          className="block md:hidden text-white text-4xl p-2 rounded-lg hover:bg-[#101820] cursor-pointer border-0"
+          onClick={() => setExpand(!expand)}
+        >
+          <CgMenuRight />
+        </div>
+      </div>
+      <div
+        className={`${
+          !expand && "h-0"
+        } h-auto overflow-hidden block md:hidden md:flex items-center text-center font-[Nunito]`}
+      >
+        <h4 className="text-white opacity-60 hover:opacity-80 transition-all duration-200 cursor-pointer px-2 py-2">
+          Location
+        </h4>
+        <h4 className="text-white opacity-60 hover:opacity-80 transition-all duration-200 cursor-pointer px-2 py-2 md:ml-10">
+          Blogs
+        </h4>
+        <h4 className="text-white opacity-60 hover:opacity-80 transition-all duration-200 cursor-pointer px-2 py-2 md:ml-10">
+          Testimonials
+        </h4>
+        <h4 className="text-white opacity-60 hover:opacity-80 transition-all duration-200 cursor-pointer px-2 py-2 md:ml-10">
+          Contact
+        </h4>
       </div>
     </nav>
   );
