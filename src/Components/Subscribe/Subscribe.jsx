56a2c35f4fd15934beaba4ea35546dfdd400ebc7
firasel <mdrasel601390@gmail.com>
2022-02-19 06:23:45 +0600

    subscribe section responsive done

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -5,17 +5,17 @@ const Subscribe = () => {
   return (
     <div className="container mx-auto w-full bg-[#6B68FF]">
       <div className="w-full py-6 md:py-28 font-[Nunito] text-center">
-        <h2 className="text-6xl text-white font-[600] mb-8 md:leading-snug">
+        <h2 className="text-4xl md:text-5xl lg:text-6xl text-white font-[600] mb-8 md:leading-snug">
           Subscribe to Our Newsletter
           <br />
           For Weekly Article Update.
         </h2>
-        <p className="mt-0 mb-8 text-white">
+        <p className="mt-0 mb-8 text-white text-base md:text-lg">
           We have smartphone-related blogs, We regularly share them on our blog
           page.
-          <br /> We can notify you via email. We will not spam you, we promise.
+          <br className="hidden md:block" /> We can notify you via email. We will not spam you, we promise.
         </p>
-        <div className="w-4/6 md:w-3/6 mx-auto flex items-center gap-4">
+        <div className="w-full sm:w-4/6 md:w-3/6 mx-auto md:flex items-center gap-4">
           <div className="flex items-center w-full text-lg bg-[#ffffff26]">
             <HiOutlineMail className="text-6xl text-white p-2" />
             <input
@@ -24,7 +24,7 @@ const Subscribe = () => {
               placeholder="Enter your e-mail address"
             />
           </div>
-          <button className="w-fit text-xl font-semibold px-12 py-4 bg-[#16222D] text-white">
+          <button className="w-full mt-5 md:mt-0 md:w-fit text-xl font-semibold px-12 py-4 bg-[#16222D] text-white">
             Subscribe
           </button>
         </div>
